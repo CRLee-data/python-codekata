9e1613ff006470333aa9e6e455877f9fc04c9e7b
CRLee-data <dcf101k2@gmail.com>
2026-01-20 11:47:06 +0900

    [프로그래머스] L0 - 편지 (#15)

---
+++ programmers/L0_기초입문/입문/015_편지.py
@@ -0,0 +1,9 @@
+# 편지
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120898
+# 알고리즘: 기초
+# 작성자: 이채리
+# 작성일: 2026. 01. 20. 11:46:58
+
+def solution(message):
+    return len(message)*2
