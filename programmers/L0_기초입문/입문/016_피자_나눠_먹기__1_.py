39b257f568c420f24e0bfcd27569cdbeebff037c
CRLee-data <dcf101k2@gmail.com>
2026-01-21 11:00:47 +0900

    [프로그래머스] L0 - 피자 나눠 먹기 (1) (#16)

---
+++ programmers/L0_기초입문/입문/016_피자_나눠_먹기__1_.py
@@ -0,0 +1,8 @@
+# 피자 나눠 먹기 (1)
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120814
+# 알고리즘: 기초
+# 작성자: 이채리
+# 작성일: 2026. 01. 21. 11:00:46
+
+solution = lambda n: n/7 if n%7 == 0 else int(n/7)+1
