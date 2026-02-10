9b575f989a1901514d63480823ccd5cae296aaa9
CRLee-data <dcf101k2@gmail.com>
2026-02-10 19:07:22 +0900

    [프로그래머스] L0 - 특정 문자 제거하기 (#32)

--- programmers/L0_기초입문/입문/032_특정_문자_제거하기.py
+++ programmers/L0_기초입문/입문/032_특정_문자_제거하기.py
@@ -3,8 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120826
 # 알고리즘: 기초
 # 작성자: 이채리
-# 작성일: 2026. 02. 10. 19:06:59
+# 작성일: 2026. 02. 10. 19:07:12
 
 def solution(my_string, letter):
-    answer = my_string.replace(letter, "")
-    return answer
+    return my_string.replace(letter, "")
