d52a598d11ba1afdb371e5d38a8caaca74b0703b
CRLee-data <dcf101k2@gmail.com>
2026-03-17 10:25:28 +0900

    [프로그래머스] L0 - 가위 바위 보 (#44)

---
+++ programmers/L0_기초입문/입문/044_가위_바위_보.py
@@ -0,0 +1,17 @@
+# 가위 바위 보
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120839
+# 알고리즘: 기초
+# 작성자: 이채리
+# 작성일: 2026. 03. 17. 10:25:25
+
+def solution(rsp):
+    answer = ''
+    for i in rsp : 
+        if i == '0' : 
+            answer += '5'
+        elif i == '2' : 
+            answer += '0'
+        else : 
+            answer += '2'
+    return answer
